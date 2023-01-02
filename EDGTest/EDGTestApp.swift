f073753a842cdb035020be86722af54b4d553175
SajithCodecraft <121784044+SajithCodecraft@users.noreply.github.com>
2023-01-02 12:57:42 +0530

    Add files via upload
    Initial commit

---
+++ EDGTest/EDGTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EDGTestApp.swift
+//  EDGTest
+//
+//  Created by sajith on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct EDGTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
